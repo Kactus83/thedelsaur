f3c1d8e0173cb46fe4f3746dbe3ae02be8b020d2
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-09-24 12:57:55 +0200

    Fix routing

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -7,19 +7,19 @@ const router = express.Router();
 // Route pour récupérer tous les utilisateurs
 router.get('/', usersController.getAllUsers);
 
-// Route pour récupérer un utilisateur par son ID
-router.get('/:id', usersController.getUserById);
-
-// Route pour récupérer un utilisateur par son email
-router.get('/email/:email', usersController.getUserByEmail);
+// Route pour récupérer le profil de l'utilisateur connecté
+router.get('/my-profile', authenticateJWT, usersController.getMyProfile);
 
 // Route pour récupérer un utilisateur par son username
 router.get('/username/:username', usersController.getUserByUsername);
 
+// Route pour récupérer un utilisateur par son email
+router.get('/email/:email', usersController.getUserByEmail);
+
+// Route pour récupérer un utilisateur par son ID
+router.get('/:id', usersController.getUserById);
+
 // Route pour supprimer un utilisateur par son ID
 router.delete('/:id', usersController.deleteUser);
-
-// Route pour récupérer le profil de l'utilisateur connecté
-router.get('/my-profile', authenticateJWT, usersController.getMyProfile);
 
 module.exports = router;
