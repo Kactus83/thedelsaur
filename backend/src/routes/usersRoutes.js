8a065946cd015936aab87782dc55be98eb84a247
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-09-25 14:04:44 +0200

    refactor backend routes

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -4,22 +4,7 @@ const authenticateJWT = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-// Route pour récupérer tous les utilisateurs
-router.get('/', usersController.getAllUsers);
-
 // Route pour récupérer le profil de l'utilisateur connecté
 router.get('/my-profile', authenticateJWT, usersController.getMyProfile);
-
-// Route pour récupérer un utilisateur par son username
-router.get('/username/:username', usersController.getUserByUsername);
-
-// Route pour récupérer un utilisateur par son email
-router.get('/email/:email', usersController.getUserByEmail);
-
-// Route pour récupérer un utilisateur par son ID
-router.get('/:id', usersController.getUserById);
-
-// Route pour supprimer un utilisateur par son ID
-router.delete('/:id', usersController.deleteUser);
 
 module.exports = router;
