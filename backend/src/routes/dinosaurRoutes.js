45caee1ee288940be719ddd2cc26122a0a49f869
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-10-01 12:12:21 +0200

    Add actions to backend for dinosaur actions

--- backend/src/routes/dinosaurRoutes.js
+++ backend/src/routes/dinosaurRoutes.js
@@ -7,4 +7,11 @@ const router = express.Router();
 // Route pour récupérer le dinosaure de l'utilisateur connecté (prévu pour utilisation future)
 router.get('/my-dinosaur', authenticateJWT, dinosaurController.getMyDinosaur);
 
+// Actions
+router.post('/actions/eat', authenticateJWT, dinosaurController.eatDinosaur);
+router.post('/actions/sleep', authenticateJWT, dinosaurController.sleepDinosaur);
+router.post('/actions/wake', authenticateJWT, dinosaurController.wakeDinosaur);
+router.post('/actions/resurrect', authenticateJWT, dinosaurController.resurrectDinosaur);
+
+
 module.exports = router;
