9389ec57af770fd65ca2d4114ff9c3c97cfe740e
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-10-15 11:03:04 +0200

    WIP

--- test/new-frontend/src/index.tsx
+++ test/new-frontend/src/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/styles.css'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si vous souhaitez mesurer les performances de votre application, passez une fonction
+// pour enregistrer les résultats (par exemple : reportWebVitals(console.log))
+// ou envoyer vers un endpoint d'analyse. En savoir plus : https://bit.ly/CRA-vitals
 reportWebVitals();
