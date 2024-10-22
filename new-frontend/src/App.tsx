029dd6338b47070f4973b1887784bb86abc7143b
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-10-22 11:58:12 +0200

    Debug JWT token management in frontend

--- new-frontend/src/App.tsx
+++ new-frontend/src/App.tsx
@@ -4,21 +4,40 @@ import HomePage from './pages/Home/HomePage';
 import DashboardPage from './pages/Dashboard/DashboardPage';
 import AdminPage from './pages/Admin/AdminPage';
 import UserProfilePage from './pages/UserProfile/UserProfilePage';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 const App: React.FC = () => {
-    const isAuthenticated = !!localStorage.getItem('token');
-
-    return (
-        <Routes>
-            <Route path="/" element={!isAuthenticated ? <HomePage /> : <Navigate to="/dashboard" />} />
-            <Route path="/dashboard" element={isAuthenticated ? <DashboardPage /> : <Navigate to="/" />} />
-            <Route path="/admin" element={isAuthenticated ? <AdminPage /> : <Navigate to="/" />} />
-            <Route path="/user-profile" element={isAuthenticated ? <UserProfilePage /> : <Navigate to="/" />} />
-            {/* Redirection pour les routes non définies */}
-            <Route path="*" element={<Navigate to="/" />} />
-            
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/user-profile"
+        element={
+          <ProtectedRoute>
+            <UserProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      {/* Redirection pour les routes non définies */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default App;
