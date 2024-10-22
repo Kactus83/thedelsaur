dbdd5076eec030d92dc616ac504f0632c75d0e04
Jsouchon <jeanmichelsouchon83170@outlook.fr>
2024-10-22 14:20:14 +0200

    responsive actualisation
    responsive dans le header

--- new-frontend/src/components/Common/Header.tsx
+++ new-frontend/src/components/Common/Header.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'; 
-const HandleLogout= () =>{
+import './Header.css';
+
+const HandleLogout = () => {
     localStorage.removeItem('token');
 };
+
 const Header: React.FC = () => {
+    const [menuOpen, setMenuOpen] = useState(false); // State to toggle menu
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen); // Toggle menu open/close
+    };
+
     return (
         <header>
             <div className="Title">
-                        IdleSaur🦖
+                IdleSaur🦖
             </div>
-            <nav>
-                            {/* <Link to="/">Accueil</Link>
-                            <Link to="/dashboard">Dashboard</Link> */}
-                            <Link to="/user-profile">Profil</Link>
-                            <Link to="/" onClick={HandleLogout}>disconnect</Link>
+            
+            {/* Hamburger icon with spans */}
+            <div className={`burger-icon ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            {/* Navigation menu */}
+            <nav className={menuOpen ? 'nav open' : 'nav'}>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/user-profile" onClick={toggleMenu}>Profil</Link>
+                <Link to="/" onClick={() => { HandleLogout(); toggleMenu(); }}>Disconnect</Link>
             </nav>
         </header>
     );
 };
 
 export default Header;
+
