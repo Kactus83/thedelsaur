5cfd3f3ef4f11dbb09920b578705f940c3fd41ae
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-11-05 13:42:36 +0100

    Add special specs for dinosaur creation

---
+++ backend/src/modules/dinosaurs/libs/dinosaur-type-multipliers.ts
@@ -0,0 +1,29 @@
+export const DinosaurTypeMultipliers = {
+    land: {
+      earn_herbi_food_multiplier: 1,
+      earn_carni_food_multiplier: 1,
+      earn_food_multiplier: 1,
+      earn_energy_multiplier: 1,
+      earn_experience_multiplier: 1,
+      max_energy_multiplier: 1,
+      max_food_multiplier: 1.5,
+    },
+    air: {
+      earn_herbi_food_multiplier: 1,
+      earn_carni_food_multiplier: 1,
+      earn_food_multiplier: 1,
+      earn_energy_multiplier: 1,
+      earn_experience_multiplier: 1,
+      max_energy_multiplier: 1.5,
+      max_food_multiplier: 1,
+    },
+    sea: {
+      earn_herbi_food_multiplier: 1,
+      earn_carni_food_multiplier: 1,
+      earn_food_multiplier: 1,
+      earn_energy_multiplier: 1,
+      earn_experience_multiplier: 1,
+      max_energy_multiplier: 1.5,
+      max_food_multiplier: 1,
+    },
+  };
