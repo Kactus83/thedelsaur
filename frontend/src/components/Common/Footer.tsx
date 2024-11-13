9f681f30b1f73ce24368f79bba3247e238137760
Jsouchon <jeanmichelsouchon83170@outlook.fr>
2024-11-13 14:00:25 +0100

    update temp

--- frontend/src/components/Common/Footer.tsx
+++ frontend/src/components/Common/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.css'; // Créez ce fichier si nécessaire
 const Footer: React.FC = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} The IdleSaur. Tous droits réservés.</p>
+            {/* <p>&copy; {new Date().getFullYear()} The IdleSaur. Tous droits réservés.</p> */}
         </footer>
     );
 };
