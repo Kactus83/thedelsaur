eb2f34ba6f841f816387846f1cbf74e3bdf51e6a
Jsouchon <jeanmichelsouchon83170@outlook.fr>
2024-11-13 14:40:34 +0100

    update taille div action

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -95,7 +95,7 @@ const DashboardPage: React.FC = () => {
             setLastEvent(event);
             setIsActionInProgress(false); // Arrête l'animation du dinosaure
             // Cache l'overlay après 3 secondes
-            setTimeout(() => setLastEvent(null), 1500);
+            setTimeout(() => setLastEvent(null), 1750);
         }, 500);
     };
 
