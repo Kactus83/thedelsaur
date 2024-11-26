4f6897a6daeb35bd754bd82eea1b423ae057bc99
Jsouchon <76991749+JeanMichelSouchon@users.noreply.github.com>
2024-11-26 09:39:00 +0100

    sentry first test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,31 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { DinosaursModule } from './modules/dinosaurs/dinosaurs.module';
+import * as Sentry from '@sentry/node';
 
 dotenv.config();
+
+Sentry.init({
+  dsn: "https://0bbb1c60a514242516015fdab9c2e06f@o4508363323211776.ingest.de.sentry.io/4508363546099792",
+  environment: "development",
+  tracesSampleRate: 1.0,
+});
+
+process.on('unhandledRejection', (reason) => {
+  Sentry.captureException(reason);
+});
+
+process.on('uncaughtException', (error) => {
+  Sentry.captureException(error);
+});
+
+
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
+// The error handler must be registered before any other error middleware and after all controllers
+Sentry.setupExpressErrorHandler(app);
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -27,7 +46,13 @@ app.use('/users', usersModule.router);
 app.use('/admin', adminModule.router);
 app.use('/dinosaurs', dinosaursModule.router);
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+
 // Démarrage du serveur
 app.listen(PORT, () => {
   console.log(`Backend server is running on http://localhost:${PORT}`);
 });
+
