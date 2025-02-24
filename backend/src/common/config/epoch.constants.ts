a78eacad763767e37f2a68d4c38f0ebb152838b6
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2025-02-24 19:42:13 +0100

    Fix dans les durées d'epoqures

--- backend/src/common/config/epoch.constants.ts
+++ backend/src/common/config/epoch.constants.ts
@@ -1,9 +1,10 @@
+import { DINOSAUR_CONSTANTS } from "./dinosaur.constants";
+
 // Definition des constantes pour les paliers
 export const EPOCH_CONSTANTS = {
-    MIN_EPOCH_DURATION: 20,      // Durée minimale de chaque palier
-    MAX_EPOCH_DURATION: 200,     // Durée maximale que chaque
-    CURVE_STEEPNESS: 1.5,        // Facteur de contrôle pour la progression de la courbe
-    INITIAL_EPOCH_DURATION_FACTOR: 10000, // Facteur de durée initial  pour correpsondre au facteur appliqué a l'age  
+    MIN_EPOCH_DURATION: 30000 * DINOSAUR_CONSTANTS.INITIAL_AGE_FACTOR,      // Durée minimale de chaque palier en ms 
+    MAX_EPOCH_DURATION: 300000 * DINOSAUR_CONSTANTS.INITIAL_AGE_FACTOR,     // Durée maximale que chaque en ms
+    CURVE_STEEPNESS: 1.5,        // Facteur de contrôle pour la progression de la courbe 
     };
 
 
